41f16eff3deb2c174203250bfbd5d4dd4445b5f9
ankitghorpade <75006687+ankitghorpade@users.noreply.github.com>
2021-06-04 13:53:07 +0530

    Addressbook

---
+++ Person.java
@@ -0,0 +1,38 @@
+import java.util.Objects;
+
+public class Person {
+    String firstName;
+    String lastName;
+
+    public String getFirstName() {
+
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return firstName.equals(person.firstName) && lastName.equals(person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
